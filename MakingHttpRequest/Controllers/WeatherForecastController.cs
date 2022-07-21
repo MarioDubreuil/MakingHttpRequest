4c74f5ff2ec258f836a6125d79d969246d730422
MarioDubreuil <mario_dubreuil@hotmail.com>
2022-07-20 20:04:35 -0400

    with using, connections are now timed_wait instead of established, better but we could still run out of sockets

--- MakingHttpRequest/Controllers/WeatherForecastController.cs
+++ MakingHttpRequest/Controllers/WeatherForecastController.cs
@@ -17,9 +17,11 @@ namespace MakingHttpRequest.Controllers
         public async Task<string> Get(string zipCode)
         {
             var url = $"https://api.zippopotam.us/us/{zipCode}";
-            var httpClient = new HttpClient();
-            var response = await httpClient.GetAsync(url);
-            return await response.Content.ReadAsStringAsync();
+            using (var httpClient = new HttpClient())
+            {
+                var response = await httpClient.GetAsync(url);
+                return await response.Content.ReadAsStringAsync();
+            }
         }
     }
 }
